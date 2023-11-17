5f412a04776266df771aa9d2b6974c043ea954f6
MorpKnight <christoffelsihombing@gmail.com>
2023-11-17 15:12:35 +0700

    CS8 - Separuh selesai

--- app/src/main/java/com/GiovanChristoffelSihombingJBusRS/jbus_android/LoginActivity.java
+++ app/src/main/java/com/GiovanChristoffelSihombingJBusRS/jbus_android/LoginActivity.java
@@ -2,13 +2,50 @@ package com.GiovanChristoffelSihombingJBusRS.jbus_android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private Button loginButton = null;
+    private TextView registerButton = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        registerButton = findViewById(R.id.create_account);
+        loginButton = findViewById(R.id.login_button);
+
+        registerButton.setOnClickListener(v -> moveActivity(LoginActivity.this, RegisterActivity.class));
+        loginButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                String email = ((TextView) findViewById(R.id.login_email)).getText().toString();
+                String password = ((TextView) findViewById(R.id.login_password)).getText().toString();
+
+                if(TextUtils.isEmpty(email) || TextUtils.isEmpty(password)){
+                    viewToast(LoginActivity.this, "Email or Password is Empty");
+                    return;
+                }
+                viewToast(LoginActivity.this, "Logged");
+                moveActivity(LoginActivity.this, MainActivity.class);
+            }
+        });
+    }
+
+    private void moveActivity(Context ctx, Class<?> cls){
+        Intent intent = new Intent(ctx, cls);
+        startActivity(intent);
+    }
+
+    private void viewToast(Context ctx, String msg){
+        Toast.makeText(ctx, msg, Toast.LENGTH_SHORT).show();
     }
 }
---
+++ app/src/main/java/com/GiovanChristoffelSihombingJBusRS/jbus_android/RegisterActivity.java
@@ -0,0 +1,48 @@
+package com.GiovanChristoffelSihombingJBusRS.jbus_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    private TextView emailEditText, usernameEditText, passwordEditText;
+    private Button registerButton = null;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        emailEditText = findViewById(R.id.register_email);
+        usernameEditText = findViewById(R.id.register_username);
+        passwordEditText = findViewById(R.id.register_password);
+        registerButton = findViewById(R.id.register_button);
+
+        registerButton.setOnClickListener(v -> {
+            String email = emailEditText.getText().toString();
+            String username = usernameEditText.getText().toString();
+            String password = passwordEditText.getText().toString();
+
+            if(email.isEmpty() || username.isEmpty() || password.isEmpty()){
+                viewToast(RegisterActivity.this, "Email, Username, or Password is Empty");
+                return;
+            }
+            viewToast(RegisterActivity.this, "Registering");
+            moveActivity(RegisterActivity.this, MainActivity.class);
+        });
+    }
+
+    private void moveActivity(Context ctx, Class<?> cls){
+        Intent intent = new Intent(ctx, cls);
+        startActivity(intent);
+    }
+
+    private void viewToast(Context ctx, String msg){
+        Toast.makeText(ctx, msg, Toast.LENGTH_SHORT).show();
+    }
+}
